f4595bb0640e135fb11a1588296ccedca6c2ac78
Haider Rathore <70682547+Haiderrathore@users.noreply.github.com>
2024-05-14 03:01:56 +0500

    Added Collection view for multiple mustaches and wrote functionallity for changing filters on the go.

---
+++ FilterCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  FilterCollectionViewCell.swift
+//  ARFaceFilter
+//
+//  Created by Haider Rathore on 14/05/2024.
+//
+
+import UIKit
+
+class FilterCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ filterCOLLECTIONCELLCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  filterCOLLECTIONCELLCollectionViewCell.swift
+//  ARFaceFilter
+//
+//  Created by Haider Rathore on 14/05/2024.
+//
+
+import UIKit
+
+class filterCOLLECTIONCELLCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
